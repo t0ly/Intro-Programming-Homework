12b41c5b765d1a4516275b8abfbba59cae98f6a3
to-ly <to-ly@users.noreply.github.com>
2015-01-12 23:29:10 +0200

    Age after 10 yrs update
    using System.Globalization;

--- Problem 15. Age after 10 Years/AgeAfter10Years.cs
+++ Problem 15. Age after 10 Years/AgeAfter10Years.cs
@@ -1,18 +1,35 @@
-﻿using System;
+﻿//Problem 15.* Age after 10 Years
+//    Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+using System;
+using System.Globalization; //
+using System.Threading;     //
 
 class AgeAfter10Years
 {
     static void Main(string[] args)
     {
+        //Set the date format
+        CultureInfo bg = new CultureInfo("bg-BG");
+        Thread.CurrentThread.CurrentCulture = bg;
+
+
+
+
+
+        //Ask user for DOB and save input into strings   
+        Console.WriteLine("What is your date of birth? ");
+        DateTime DOB = DateTime.Parse(Console.ReadLine());
         
-        //Ask user for DOB
-        Console.WriteLine("What is your date of birth (MM/DD/YYYY)?");
-        string DOBunparsed = Console.ReadLine();
-        DateTime DOBparsed;
-        DateTime.TryParse(DOBunparsed, out DOBparsed);
-        Console.WriteLine(DOBparsed);
+        //Calculate birthdate
+        int Age = DateTime.Now.Year - DOB.Year;
+        //Check if birthday didn't pass
+        if (DateTime.Now.Month < DOB.Month && DateTime.Now.Day < DOB.Day)
+            Age--;
         
-        Console.WriteLine("Your age now is " + (System.DateTime.Now.Date - DOBparsed));
+        //Tell the user her age
+        Console.WriteLine("Your age now is {0}", Age);
+        Console.WriteLine("Your age in 10 years will be {0}", (Age + 10));
 
     }
 }

79d5c03a0c64f744d40e5318d6c595169cc2cb1a
to-ly <to-ly@users.noreply.github.com>
2015-01-30 00:02:58 +0200

    11 Number as words

--- 05.Conditional Statements/11.Number as Words/NumbersAsWords.cs
+++ 05.Conditional Statements/11.Number as Words/NumbersAsWords.cs
@@ -19,47 +19,78 @@ namespace _11.Number_as_Words
         static void Main(string[] args)
         {
             Console.Write("Enter a number to spell it out: ");
-            int number = int.Parse(Console.ReadLine());
+            
+            int number = int.Parse(Console.ReadLine());            
+            int len = number.ToString().Length;
             string words = "";
 
-            if (number == 0)
+            switch (len)
             {
-                words = "Zero";
-            } 
-            else if (number < 20)
-            {
-                words = ToWord(number);
-            }        
-            else if(number < 100)
-            {
-                words = ToWord((number / 10) * 10) + " " +  ToWord(number % 10);                 
+                case 1:
+                  
+                    words = (number == 0) ? "Zero" : Digits(number);
+                    break;
+
+                case 2: //10 to 99
+                    if (number < 20)
+                    {
+                        words = Digits(number);
+                    } 
+                    else 
+                    {
+                        words = Tens(number) + " " + Digits(number);
+                    }
+                    break;
+                
+
+                case 3:
+                    if (number % 100 == 0)
+                    {
+                        words = Digits(number / 100) + "hundered";
+                    }
+                    else
+                    {
+                        words = Tens(number / 10) + Digits(number / 100) + "hundered";
+                    }
+                    break;
+
+                default:
+                    break;
             }
-            else if (number < 1000)
-            {
-                words = ToWord(number / 100) + " Hundred " + ToWord((number%100 / 10) * 10) + " " + ToWord(number % 10);
-            }
-            else if (number < 1000000)
-            {
-                                               
-                words = ToWord(number / 1000) + " Thousand " + ToWord(((number % 1000) / 100)) + 
-                    " Hundred " + ToWord((number % 100 / 10) * 10) + " " + ToWord(number % 10);
-            }
-            else  {
-                words = "Gaziloopon and thirty-three. No, really I don't know to count that high!";
-            }
+            
+            //if (number == 0)
+            //{
+            //    words = "Zero";
+            //}
+            //else if (number < 20)
+            //{
+            //    words = Digits(number);
+            //}
+            //else if (number < 100)
+            //{
+            //    words = Tens(number) + Digits(number) ;
+            //}
+            //else if (number < 1000)
+            //{
+            //    words = Hundreds(number) + " " + Tens(number) + Digits(number);
+            //}
+      
+            
+
+      
 
             Console.WriteLine(words);
 
         }
 
-        public static string ToWord(int nr)
+        public static string Digits(int nr)
         {
 
             string word = "";
 
             switch (nr)
             {
-                //case 0: word += "and"; break;
+                case 0: word += "and"; break;
                 case 1: word += "One"; break;
                 case 2: word += "Two"; break;
                 case 3: word += "Three"; break;
@@ -69,15 +100,41 @@ namespace _11.Number_as_Words
                 case 7: word += "Seven"; break;
                 case 8: word += "Eight"; break;
                 case 9: word += "Nine"; break;
+
                 case 10: word += "Ten"; break;
                 case 11: word += "Eleven"; break;
                 case 12: word += "Twelve"; break;
                 case 13: word += "Thirteen"; break;
                 case 14: word += "Fourteen"; break;
-                case 15: word += "Sixteen"; break;
-                case 16: word += "Seventeen"; break;
-                case 17: word += "Eighteen"; break;
-                case 18: word += "Nineteen"; break;
+                case 15: word += "Fifteen"; break;
+                case 16: word += "Sixteen"; break;
+                case 17: word += "Seventeen"; break;
+                case 18: word += "Eighteen"; break;
+                case 19: word += "Nineteen"; break;
+
+                case 20: word += "Twenty"; break;
+                case 30: word += "Thirty"; break;
+                case 40: word += "Forty"; break;
+                case 50: word += "Fifty"; break;
+                case 60: word += "Sixty"; break;
+                case 70: word += "Seventy"; break;
+                case 80: word += "Eighty"; break;
+                case 90: word += "Ninety"; break;
+
+                default: break;
+
+            }
+
+            return word;
+        }
+
+        public static string Tens(int nr)
+        {
+
+            string word = "";
+
+            switch (nr)
+            {
                 case 20: word += "Twenty"; break;
                 case 30: word += "Thirty"; break;
                 case 40: word += "Forty"; break;
@@ -89,9 +146,30 @@ namespace _11.Number_as_Words
                 default: break;
 
             }
-
             return word;
+
         }
 
+        public static string Hundreds(int nr)
+        {
+            
+            string word = "";
+            switch (nr)
+            {
+                case 100: word += "One Hundred"; break;
+                case 200: word += "Two Hundred"; break;
+                case 300: word += "Three Hundred"; break;
+                case 400: word += "Four Hundred"; break;
+                case 500: word += "Five Hundred"; break;
+                case 600: word += "Six Hundred"; break;
+                case 700: word += "Seven Hundred"; break;
+                case 800: word += "Eight Hundred"; break;
+                case 900: word += "Nine Hundred"; break;
+                
+                default:
+                    break;
+            }
+            return word;
+        }
     }
 }

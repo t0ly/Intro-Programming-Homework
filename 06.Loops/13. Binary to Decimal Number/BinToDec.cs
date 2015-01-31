7bf9da3b39940715a48f46a62e975e279c56ebc2
to-ly <to-ly@users.noreply.github.com>
2015-01-31 23:36:12 +0200

    all required for loops

---
+++ 06.Loops/13. Binary to Decimal Number/BinToDec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Binary_to_Decimal_Number
+{
+    class BinToDec
+    {
+        /// <summary>
+        /// Using loops write a program that converts a binary integer number to its decimal form.
+        /// The input is entered as string. The output should be a variable of type long.
+        /// Do not use the built-in .NET functionality.
+
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            long decimalresult = 0;
+
+            Console.WriteLine("converts a binary integer number to its decimal form.");
+            Console.Write("Enter binary integer: ");
+
+
+            string input = Console.ReadLine().Trim();
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                decimalresult += long.Parse(input[i].ToString()) * (long)Math.Pow(2, input.Length - (i + 1));
+                Console.WriteLine(long.Parse(input[i].ToString()) + "\t" + "2^" + (input.Length - (i + 1)) + " \t " + decimalresult);
+            }
+
+            Console.WriteLine("Binary {0} = decimal {1}", input, decimalresult);
+
+
+        }
+    }
+}
---
+++ 06.Loops/15. Hexadecimal to Decimal Number/HexToDec.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Hexadecimal_to_Decimal_Number
+{
+    class HexToDec
+    {
+
+        /// <summary>
+        /// 15. Hexadecimal to Decimal 
+        /// Using loops write a program that converts a hexadecimal integer number to its decimal form
+        /// The input is entered as string. The output should be a variable of type long
+        /// Do not use the built-in .NET functionality.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            long decNr = 0;
+
+            Console.WriteLine("converts a hexadecimal integer number to its decimal form.");
+            Console.Write("Enter hex integer: ");
+
+
+            string input = Console.ReadLine().Trim();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                decNr += DeHex(input[i].ToString()) * (long)Math.Pow(16, input.Length - (i + 1));
+
+            }
+
+            Console.WriteLine("hex {0} = decimal {1}", input, decNr);
+
+
+
+        }
+
+        static int DeHex(string hexnumber)
+        {
+
+            int decimalnr = 0;
+
+            switch (hexnumber)
+            {
+                case "0": decimalnr = 0; break;
+                case "1": decimalnr = 1; break;
+                case "2": decimalnr = 2; break;
+                case "3": decimalnr = 3; break;
+                case "4": decimalnr = 4; break;
+                case "5": decimalnr = 5; break;
+                case "6": decimalnr = 6; break;
+                case "7": decimalnr = 7; break;
+                case "8": decimalnr = 8; break;
+                case "9": decimalnr = 9; break;
+                case "A": decimalnr = 10; break;
+                case "B": decimalnr = 11; break;
+                case "C": decimalnr = 12; break;
+                case "D": decimalnr = 13; break;
+                case "E": decimalnr = 14; break;
+                case "F": decimalnr = 15; break;
+
+                default: break;
+            }
+
+            return decimalnr;
+
+        }
+
+
+    }
+}

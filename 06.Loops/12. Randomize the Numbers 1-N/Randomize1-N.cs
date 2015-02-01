1b8d823ed8354c201336903e9de886fcdb541f16
to-ly <to-ly@users.noreply.github.com>
2015-02-01 22:17:41 +0200

    Loops 17

---
+++ 06.Loops/12. Randomize the Numbers 1-N/Randomize1-N.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Randomize1N
+    {
+    /// <summary>
+    ///  Write a program that enters in integer n and prints the numbers 1, 2, …, n in random order
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+        {
+
+        //get n from user        
+        Console.WriteLine("prints the numbers 1, 2, …, n in random order");
+        Console.Write("Enter integer n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        //initialize the numbers array, sort array, and randomness
+        int[] numbersArray = new int[n];       
+        double[] sortArray = new double[n];    
+        Random rnd = new Random();             
+
+        //populate the two arrays
+        for(int i = 0; i < n; i++)
+            {
+            sortArray[i] = rnd.NextDouble();
+            numbersArray[i] = i + 1;
+            }
+
+        //this is what does the trick
+        Array.Sort(sortArray, numbersArray);
+
+        //output the random order
+        foreach(var nr in numbersArray)
+            {
+            Console.Write("{0,2}   ", nr);
+            }
+        
+        //clear the console
+        Console.WriteLine();
+
+
+        }
+    }
---
+++ 06.Loops/17.+ Calculate GCD/CalculateGCD.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+class CalculateGCD
+{
+    /// <summary>
+    /// 17.* Calculate GCD
+
+    /// Write a program that calculates the greatest common divisor (GCD) of given two integers a and b.
+    /// Use the Euclidean algorithm (find it in Internet).
+
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+        {
+
+ 
+        // our numbers
+        int a = 1;
+        int b = 1;
+        
+        
+        //get input        
+        Console.WriteLine("Calculates the greatest common divisor (GCD) of given two integers a and b.");
+        while(true)
+        {
+            Console.Write("a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = int.Parse(Console.ReadLine());
+            if(a != b && a > 0 && b > 0)
+            {
+               break; 
+            }
+        }
+
+   
+        if(a < b)  //put the larger first
+        {
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a = {0}; b = {1}", a, b);
+        }
+
+        int remainder = 1;
+ 
+        // our formula is 
+        //a = quotent * b + remainder
+        //
+        //a = q0 b + r0
+        //b = q1 r0 + r1
+        //r0 = q2 r1 + r2
+        //r1 = q3 r2 + r3
+
+        Console.WriteLine("Calculating...");
+        while( remainder != 0)
+        {
+            remainder = a % b;
+            a = b;
+            b = remainder;
+
+        }
+
+
+          Console.WriteLine("Greatest Common Denominator is {0}", a);
+        }
+}
